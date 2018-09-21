8ee0530ef196a8382e4a47335a1f5aab7e0fcd79
Euxiac <32811534+Euxiac@users.noreply.github.com>
2018-09-21 20:57:10 +0800

    added start n end menus

---
+++ RiverMessage/Assets/Scripts/BottleReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BottleReader : MonoBehaviour {
+	public GameObject Message1;
+	//private bool touchingbottle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Message1.SetActive(true);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		Message1.SetActive(false);
+	}
+}
---
+++ RiverMessage/Assets/Scripts/StartGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+	//public string button;
+	public string scenetoload;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown(KeyCode.Return))
+	{
+		  SceneManager.LoadScene(scenetoload);
+	}	
+	}
+}
